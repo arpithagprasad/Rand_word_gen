83d214a3e689c8ab5f050c6960ec45ee93ddd832
ARPITHA <arpithagprasad@gmail.com>
2022-08-03 15:45:44 +0530

    Second Commit

---
+++ app/controllers/word_controller.rb
@@ -0,0 +1,31 @@
+class WordController < ApplicationController
+    def index
+      @words = Word.last(5)
+    end
+  
+    def error; end
+  
+    def new
+      @word = Word.find_by(name: params[:q])
+      if @word
+        redirect_to action: "show", id: @word.id
+      else
+        if WordSearch.new(word_params).call
+          @word =WordSearch.new(word_params).call
+          redirect_to action: "show", id: @word.id
+        else
+          redirect_to error_path
+        end
+      end
+    end
+  
+    def show
+      @word = Word.find_by(id: params[:id])
+    end
+  
+    private
+  
+    def word_params
+      params.permit(:endpoint, :q, :authenticity_token, :commit)
+    end
+  end
